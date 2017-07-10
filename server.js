c89f3e1865e73ad54d67041b5731e45fa5c0d9b0
System <none>
2017-07-10 02:56:03 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,16 +4,18 @@
 // init project
 var express = require('express');
 var app = express();
+var useragent = require('useragent');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+useragent(true);
 
 app.all("/*", function (request, response) {
-  var ua = request.headers['user-agent'];
-  response.send(ua);
+  var agent = useragent.parse(request.headers['user-agent']);
+  response.send(agent.toAgent());
 });
 
 // listen for requests :)
